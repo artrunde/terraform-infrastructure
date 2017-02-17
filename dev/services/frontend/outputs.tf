67a5bb1dcb6292b9413aad235008cd839b17e7e5
Karsten Jakobsen <kj@evercall.dk>
2017-02-17 11:08:42 +0100

    New outputs

--- dev/services/frontend/outputs.tf
+++ dev/services/frontend/outputs.tf
@@ -16,3 +16,11 @@ output "s3_www_bucket_domain_name" {
 output "s3_assets_bucket_domain_name" {
   value = "${module.frontend-static.s3_assets_bucket_domain_name}"
 }
+
+output "s3_www_bucket_id" {
+  value = "${module.frontend-static.s3_www_bucket_id}"
+}
+
+output "s3_assets_bucket_id" {
+  value = "${module.frontend-static.s3_assets_bucket_id}"
+}
