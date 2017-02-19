4e5d445a44c8f3837c80e28bbeda8ba313234061
Karsten Jakobsen <kj@evercall.dk>
2017-02-19 21:33:47 +0100

    Added simple hello API, that invokes lambda function

---
+++ dev/services/backend/api/hello-lambda/v1/vars.tf
@@ -0,0 +1,13 @@
+variable "aws_account_id" {
+  description = "AWS account id"
+}
+
+variable "aws_profile" {
+  description = "AWS secret key"
+  default     = "artrunde"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "eu-west-1"
+}
