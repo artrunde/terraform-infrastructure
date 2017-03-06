8eb11b7ec3827f8fab5969397a6b5aef20d58c69
Karsten Jakobsen <kj@evercall.dk>
2017-03-06 10:48:42 +0100

    Added new demo api

---
+++ dev/services/backend/api/demo-api/v1/dynamodb.tf
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------------------------
+# TEST API - DYNAMODB
+# ------------------------------------------------------------------------------
+
+resource "aws_dynamodb_table" "basic_dynamodb_table" {
+
+  name = "subscribers"
+
+  read_capacity   = 3
+  write_capacity  = 3
+
+  hash_key = "email"
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  tags {
+    name = "dynamo-table-subscribers"
+    env = "dev"
+  }
+}
