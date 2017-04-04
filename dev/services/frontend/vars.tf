ef464b1c0d37bc57f361ffa93a9dd7a5f2ca7cc0
Karsten Jakobsen <kj@evercall.dk>
2017-04-04 12:27:11 +0200

    Deleted old module

--- dev/services/frontend/vars.tf
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# ---------------------------------------------------------------------------------------------------------------------
-
-variable "circleci_frontend_user" {
-  description = "Username for the CircleCI user to access frontend (Get this from KeePass)"
-}
